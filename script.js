5382eb5eb3e3d5ebc112c72930db8c67bb571257
Ali Abdulaziz <ali.abdulaziz@gomycode.co>
2021-06-07 21:25:44 +0300

    initial

---
+++ script.js
@@ -0,0 +1,24 @@
+// var: is a keyword used to tell javascript that what comes after is a variable.
+// so x: is a variable
+// the default value for any variable in javascript is undefined.
+
+var age;
+
+// How to initialize the variable?
+// you just give it a value.
+
+age = 30;
+
+// if I want to define a variable and initialize it at the same time:
+var x = 10;
+
+// I can change the value whenever I want
+x = 50;
+
+x = 30;
+
+// I can change the data type too.
+
+x = "hello world!";
+
+
